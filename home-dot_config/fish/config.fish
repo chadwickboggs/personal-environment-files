b455c7a9eccf5763d48fff96b9d2cd7ed741eb7e
Chadwick Boggs <Chadwick.Boggs@charter.com>
2016-01-21 11:51:04 -0700

    Updates.

--- home-dot_config/fish/config.fish
+++ home-dot_config/fish/config.fish
@@ -54,7 +54,7 @@ alias sshfss='sshfs -C -o workaround=all -o reconnect'
 alias grep='grep 2>/dev/null'
 alias mtail='multitail'
 alias mtailm='multitail --mergeall'
-alias brotli='/opt/workspace/brotli/tools/bro'
+alias brotli='/opt/workspace/thrid_party/brotli/tools/bro'
 
 #
 # Load environment specific files if exist.
